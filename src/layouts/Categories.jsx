0c1a751b9036530921977d218113652fc78b5f2b
MUENBI <MUENBI@users.noreply.github.com>
2021-06-06 01:03:54 +0300

    React-5Haziran-campProject-first

---
+++ src/layouts/Categories.jsx
@@ -0,0 +1,21 @@
+/* ! dosyaların uzantılarının ".js" den hiçbir farkı bulunmamaktadır. */
+import React from "react";
+import { Menu } from 'semantic-ui-react'
+
+export default function Categories() {
+  return (
+    <div>
+      <Menu pointing vertical>
+        <Menu.Item
+          name="home"
+        />
+        <Menu.Item
+          name="messages"
+        />
+        <Menu.Item
+          name="friends"         
+        />
+      </Menu>
+    </div>
+  );
+}
---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,26 @@
+/* ! dosyaların uzantılarının ".js" den hiçbir farkı bulunmamaktadır. */
+import React from "react";
+import CartSummary from "./CartSummary";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+export default function Navi() {
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
+
+          <Menu.Menu position="right">
+            <CartSummary>
+                
+            </CartSummary>
+            <Menu.Item>
+              <Button primary>Sign Up</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
